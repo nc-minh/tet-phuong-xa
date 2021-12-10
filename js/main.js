bd6ed8bdc254901973d688151959b5dbfb50a1a8
nc-minh <nguyencongminh.official@gmail.com>
2021-12-10 21:34:20 +0700

    :construction: Work in progress

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function show(){
+    var headerM = document.getElementById('header__mobile')
+
+    headerM.style.display = 'block'
+}
+
+function hide(){
+    var headerM = document.getElementById('header__mobile')
+
+    headerM.style.display = 'none'
+}
